9ff51520139ac6ac24bc1dfdb451af06b078208c
M. Hyvarinen <54714032+marhyvar@users.noreply.github.com>
2020-02-16 16:25:25 +0200

    add test scenario for unsuccesfully created user trying to login

--- viikko3/WebLogin/src/test/java/ohtu/Stepdefs.java
+++ viikko3/WebLogin/src/test/java/ohtu/Stepdefs.java
@@ -43,6 +43,18 @@ public class Stepdefs {
 		element.click();
     }
     
+	@Given("user with username {string} and password {string} is tried to be created")
+    public void userWithUsernameAndPasswordIsTriedToBeCreated(String username, String password) {
+        driver.get(baseUrl);
+        WebElement element = driver.findElement(By.linkText("register new user"));       
+        element.click();
+		
+		signUpWith(username, password, password);
+		element = driver.findElement(By.linkText("back to home"));		
+		element.click();
+    }
+
+	
     @When("correct username {string} and password {string} are given")
     public void correctUsernameAndPasswordAreGiven(String username, String password) {
         logInWith(username, password);

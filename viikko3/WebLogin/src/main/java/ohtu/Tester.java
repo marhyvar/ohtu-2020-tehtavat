8264806248543b186d18cec7a698d2e314b97f20
M. Hyvarinen <54714032+marhyvar@users.noreply.github.com>
2020-02-16 11:26:50 +0200

    fix selenium issue and go through two scenarios

--- viikko3/WebLogin/src/main/java/ohtu/Tester.java
+++ viikko3/WebLogin/src/main/java/ohtu/Tester.java
@@ -14,16 +14,19 @@ public class Tester {
         
         sleep(2);
         
-        WebElement element = driver.findElement(By.linkText("login"));
+		WebElement element = driver.findElement(By.linkText("register new user"));
         element.click();
-
-        sleep(2);
-
-        element = driver.findElement(By.name("username"));
-        element.sendKeys("pekka");
+		
+		sleep(2);
+		
+		element = driver.findElement(By.name("username"));
+        element.sendKeys("anja");
         element = driver.findElement(By.name("password"));
-        element.sendKeys("akkep");
-        element = driver.findElement(By.name("login"));
+		element.sendKeys("salaisuus");
+		element = driver.findElement(By.name("passwordConfirmation"));
+		element.sendKeys("salaisuus");
+		
+		element = driver.findElement(By.name("signup"));
         
         sleep(2);
         element.submit();
@@ -31,6 +34,25 @@ public class Tester {
         sleep(3);
         
         driver.quit();
+		
+        /*WebElement element = driver.findElement(By.linkText("login"));
+        element.click();
+
+        sleep(2);
+
+        element = driver.findElement(By.name("username"));
+        element.sendKeys("pekka");
+        element = driver.findElement(By.name("password"));
+		element.sendKeys("akkeppp");
+        //element.sendKeys("akkep");
+        element = driver.findElement(By.name("login"));
+        
+        sleep(2);
+        element.submit();
+
+        sleep(3);
+        
+        driver.quit();*/
     }
     
     private static void sleep(int n){

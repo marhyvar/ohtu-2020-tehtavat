1ebe85eddf22d759ffadd6a702894dce0d3be376
M. Hyvarinen <54714032+marhyvar@users.noreply.github.com>
2020-03-06 10:17:03 +0200

    Add class Or that checks if at least one match is true and returns true

--- viikko6/QueryLanguage/src/main/java/statistics/Main.java
+++ viikko6/QueryLanguage/src/main/java/statistics/Main.java
@@ -14,7 +14,7 @@ public class Main {
         /*Matcher m = new And( new HasAtLeast(5, "goals"),
                              new HasAtLeast(5, "assists"),
                              new PlaysIn("PHI")
-        );*/
+        );
         
         Matcher y = new And( 
         	    new Not( new HasAtLeast(1, "goals") ), 
@@ -24,14 +24,25 @@ public class Main {
         Matcher m = new And( 
         	    new HasFewerThan(1, "goals"), 
         	    new PlaysIn("NYR")
-        );
-
+        );*/
+		Matcher m = new Or( new HasAtLeast(20, "goals"),
+			new HasAtLeast(20, "assists")
+		);
+		
+		Matcher y = new And(
+			new HasAtLeast(20, "points"),
+			new Or( 
+				new PlaysIn("NYR"),
+				new PlaysIn("NYI"),
+				new PlaysIn("NJD")
+			)
+		);
         
-        for (Player player : stats.matches(y)) {
+        for (Player player : stats.matches(m)) {
             System.out.println(player);
         }
-        System.out.println("Seuraava sama tulos: ");
-        for (Player player : stats.matches(m)) {
+        System.out.println("Seuraava tulos: ");
+        for (Player player : stats.matches(y)) {
             System.out.println(player);
         }
     }

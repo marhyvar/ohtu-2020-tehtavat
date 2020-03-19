56c2a451516af05a089108c0c2493846e458d882
M. Hyvarinen <54714032+marhyvar@users.noreply.github.com>
2020-03-19 12:32:37 +0200

    Continue

--- viikko7/KiviPaperiSakset/src/main/java/ohtu/kivipaperisakset/Peli.java
+++ viikko7/KiviPaperiSakset/src/main/java/ohtu/kivipaperisakset/Peli.java
@@ -9,6 +9,10 @@ public abstract class Peli {
 		this.tuomari = tuomari;
 	}
 	
+	/*public Peli(IO IO, Tuomari tuomari, Tekoaly tekoaly) {
+		
+	}*/
+	
 	protected static boolean onkoOkSiirto(String siirto) {
         return "k".equals(siirto) || "p".equals(siirto) || "s".equals(siirto);
     }
